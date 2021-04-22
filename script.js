7a11f8e9c08b556d482f9ca6e79104275055ab5a
Tallon Hodge <tallonkh@gmail.com>
2021-04-22 17:25:50 -0500

    redid sort brush

--- script.js
+++ script.js
@@ -31,8 +31,7 @@ const dataManipFunc = (ctx, dims, scale, func) => {
   const imageData = ctx.getImageData(
     cdx, cdy,
     ~~clamp(dx + w, 0, can.width) - cdx, ~~clamp(dy + h, 0, can.height) - cdy);
-  func(imageData);
-  ctx.putImageData(imageData, cdx, cdy);
+  ctx.putImageData(func(imageData) ?? imageData, cdx, cdy);
 }
 const brushes = [{
     name: "Cursor",
@@ -136,7 +135,7 @@ const brushes = [{
     buttonTint: filterTint,
     drawFunc: (ctx, brush) => dataManipFunc(ctx, brush.dims, brush.scale, (data) => {
       const raw = data.data;
-      for (let i = 0; i < raw.length - 4; i++) {
+      for (let i = 0; i < raw.length - 7; i++) {
         raw[i] = raw[i + 4];
         raw[i + 1] = raw[i + 5];
         raw[i + 2] = raw[i + 6];
@@ -185,26 +184,31 @@ const brushes = [{
     buttonTint: filterTint,
     drawFunc: (ctx, brush) => dataManipFunc(ctx, brush.dims, brush.scale, (data) => {
       const raw = data.data;
-      for (let x = 0; x < data.width; x++) {
-        for (let y = 0; y < data.height - 1; y++) {
+      for (let x = 0; x < data.width - 1; x++) {
+        for (let y = Math.round(Math.random()); y < data.height - 1; y+=2) {
           const i1 = (x + y * data.width) * 4;
-          const i2 = i1 + data.width * 4;
+          const i2 = (x + (y+1) * data.width) * 4;
+          if(raw[i1+3] < 50 || raw[i2+3] < 50){
+            continue;
+          }
 
-          const hsl1 = rgb_hsl(raw[i1] / 255, raw[i1 + 1] / 255, raw[i1 + 2] / 255);
-          const hsl2 = rgb_hsl(raw[i2] / 255, raw[i2 + 1] / 255, raw[i2 + 2] / 255);
+          const hsl1 = rgb_hsl(raw[i1 + 0] / 255, raw[i1 + 1] / 255, raw[i1 + 2] / 255);
+          const hsl2 = rgb_hsl(raw[i2 + 0] / 255, raw[i2 + 1] / 255, raw[i2 + 2] / 255);
 
-          if ((hsl1[0] - hsl2[0] || hsl1[1] - hsl2[1] || hsl1[2] - hsl2[2]) < 0) {
-            const r1 = raw[i1];
-            const g1 = raw[i1 + 1];
-            const b1 = raw[i1 + 2];
-            const a = Math.max(raw[i1 + 3], raw[i2 + 3]);
-            raw[i1] = raw[i2];
+          if ((raw[i1 + 3] - raw[i2 + 3] || hsl1[0] - hsl2[0] || hsl1[1] - hsl2[1] || hsl1[2] - hsl2[2]) < 0) {
+            const r = raw[i1 + 0];
+            const g = raw[i1 + 1];
+            const b = raw[i1 + 2];
+            const a = raw[i1 + 3];
+            // const a = Math.max(raw[i1 + 3], raw[i2 + 3])
+            raw[i1 + 0] = raw[i2 + 0];
             raw[i1 + 1] = raw[i2 + 1];
             raw[i1 + 2] = raw[i2 + 2];
-            raw[i1 + 3] = a;
-            raw[i2] = r1;
-            raw[i2 + 1] = g1;
-            raw[i2 + 2] = b1;
+            raw[i1 + 3] = raw[i2 + 3];
+
+            raw[i2 + 0] = r;
+            raw[i2 + 1] = g;
+            raw[i2 + 2] = b;
             raw[i2 + 3] = a;
           }
         }
